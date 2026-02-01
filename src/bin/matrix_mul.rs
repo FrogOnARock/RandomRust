d9b53c33f62a9370509231204cd21aaefde086ef
frogonarock <mac2rock@gmail.com>
2026-01-31 23:42:41 -0500

    updates

--- src/bin/matrix_mul.rs
+++ src/bin/matrix_mul.rs
@@ -9,12 +9,12 @@ struct Vector<const N: usize> {
 }
 
 impl <const M: usize, const N: usize> Matrix<M, N> {
-    fn mul(&self, v: &Vector<N>) -> Vector<N> {
-        let mut result = [0.0; N];
+    fn mul(&self, v: &Vector<N>) -> Vector<M> {
+        let mut result = [0.0; M];
 
         for i in 0..M {
             for j in 0..N {
-                result[j] += self.data[i][j] * v.data[j]
+                result[i] += self.data[i][j] * v.data[j]
             }
         };
 
@@ -22,8 +22,8 @@ impl <const M: usize, const N: usize> Matrix<M, N> {
     }
 }
 fn main () {
-    let m1: Matrix<3, 2> = Matrix{ data: [[2.0, 3.0], [1.0, 2.0], [3.0, 6.0]] };
+    let m1: Matrix<3, 2> = Matrix{ data: [[2.0, 3.0], [1.0, 2.0], [1.0, 6.0]] };
     let v1: Vector<2> = Vector{ data: [1.0, 2.0] };
-    let ov1 = m1.mul(&v1);
+    let ov1: Vector<3> = m1.mul(&v1);
     println!("{:?}", ov1.data);
 }
